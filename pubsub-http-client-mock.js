01498895a7591a6bc6a222d2f22af5d756b6c33c
Esteban Abaroa <estebanabaroa@protonmail.com>
2023-03-13 05:34:22 +0000

    init

---
+++ pubsub-http-client-mock.js
@@ -0,0 +1,54 @@
+const http = require('http')
+const IoServer = require('socket.io')
+const {io} = require('socket.io-client')
+const port = 25963
+
+// try creating the websocket client, will only work in node and if not already running, but that's ok
+let server
+let ioServer
+const peers = []
+try {
+  server = http.createServer()
+  server.on('error', e => console.log(`didn't create the socket.io server: ${e.message}`))
+  ioServer = IoServer(server)
+  server.listen(port, console.log)
+  ioServer.on('connection', peer => {
+    // save the peer
+    peers.push(peer)
+    // relay messages to all other peers
+    peer.onAny((topic, message) => {
+      for (const peer of peers) {
+        peer.emit(topic, message)
+      }
+    })
+    console.log('connection')
+  })
+}
+catch (e) {
+  console.log(`didn't create the socket.io server: ${e.message}`)
+}
+
+// the ioClient is required in the browser and node
+const ioClient = socket = io(`ws://localhost:${port}`)
+
+class IpfsHttpClient {
+  constructor() {
+    this.callbacks = []
+  }
+  async publish(topic, message) {
+    ioClient.emit(topic, message)
+  }
+  subscribe(topic, callback) {
+    ioClient.on(topic, callback)
+    this.callbacks.push(callback)
+  }
+  unsubscribe(topic) {
+    for (const callback of this.callbacks) {
+      ioClient.off(topic, callback)
+    }
+  }
+}
+
+const create = () => new IpfsHttpClient()
+
+module.exports = {create}
---
+++ test.js
@@ -0,0 +1,21 @@
+const {create} = require('./pubsub-http-client-mock')
+
+pubsubClient = create()
+
+const peerId = Math.random().toString().replace('0.', '').substring(0, 4)
+
+pubsubClient.subscribe('test topic', console.log)
+pubsubClient.subscribe('test topic 2', console.log)
+
+
+setInterval(() => {
+  pubsubClient.publish('test topic', `this is peer ${peerId} on 'test topic' at ${new Date().toString().split(' ')[4]}`)
+}, 1000)
+
+setInterval(() => {
+  pubsubClient.publish('test topic 2', `this is peer ${peerId} on 'test topic 2' at ${new Date().toString().split(' ')[4]}`)
+}, 2000)
+
+setInterval(() => {
+  pubsubClient.publish('not subbed', `you shouldnt see this`)
+}, 3000)
